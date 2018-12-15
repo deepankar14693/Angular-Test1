39ffc22889b5487365cd461daabd7b0332f76640
deepankar14693 <acstar14693@gmail.com>
2018-12-15 08:36:11 +0530

    partially completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { signup } from './models/signup'
+import { Login } from './models/login';
+import { FormsModule, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,59 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
   title = 'Test1';
+  signupForm: boolean = true;
+  loginForm: boolean = false;
+  invalid: boolean = false;
+
+  signUp: signup = {
+    name: "",
+    email: "",
+    password: ""
+  };
+
+  loginModel: Login = {
+    email: "",
+    password: ""
+  };
+
+  register(): void {
+
+    console.log(this.signUp);
+
+    localStorage.setItem("user", JSON.stringify(this.signUp));
+
+    var resetForm = <HTMLFormElement>document.getElementById("register");
+    resetForm.reset();
+
+    this.signupForm = false;
+    this.loginForm = true;
+  }
+
+  login() {
+
+    var retrievedObject = localStorage.getItem("user");
+    var obj =  JSON.parse(retrievedObject);
+   // console.log("retrieved object: " + JSON.parse(retrievedObject));
+   if(obj.email === this.loginModel.email && obj.password === this.loginModel.password)
+   {
+    this.invalid = false;
+   }
+   else {
+     this.invalid = true;
+   }
+
+  }
+
+  displaySignup(): void {
+    this.loginForm = false;
+    this.signupForm = true;
+  }
+
+  displayLogin(): void {
+    this.signupForm = false;
+    this.loginForm = true;
+  }
+
 }
